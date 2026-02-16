bdb5c14093bbbfda74e967322f2064d0706ea1e3
daanyliuk <danyliuk19@ukr.net>
2026-02-16 14:58:36 +0200

    Update project.sql

--- project.sql
+++ project.sql
@@ -35,35 +35,46 @@ group by video_category_id
 order by num_of_videos desc
 ;
 
--- #1.1 Correlation between views and likes.
-SELECT
-  round((AVG(video_view_count * video_like_count) - AVG(video_view_count) * AVG(video_like_count)) /
-  (SQRT(AVG(video_view_count * video_view_count) - AVG(video_view_count) * AVG(video_view_count)) *
-   SQRT(AVG(video_like_count * video_like_count) - AVG(video_like_count) * AVG(video_like_count))),1) AS correlation
-from youtube_trending_videos_global ytvg
-where video_category_id is not null 
-;
 
 -- #2 Performance by Duration, AVG Views, Likes, and Comments per category.
+with buckets as(
+			select 
+				*
+				, case 	
+					when video_duration_sec < 60 then 'less than 1 min'
+					when video_duration_sec between 60 and 300 then '1-5 min'
+					when video_duration_sec between 301 and 600 then '5-10 min'
+					when video_duration_sec between 601 and 1800 then '10-30 min'
+					when video_duration_sec between 1801 and 3600 then '30-60 min'
+					when video_duration_sec between 3601 and 7200 then '1-2 hours'
+					when video_duration_sec between 7201 and 10800 then '2-3 hours'
+					when video_duration_sec between 10801 and 21600 then '3-6 hours'
+					when video_duration_sec > 21600  then '6 hours +'
+				end as time_category
+			from youtube_trending_videos_global
+			where video_trending_country = 'Ukraine'
+				and video_category_id is not null
+)
+
+, ranked_table as(
+				select *
+					, row_number() over(partition by time_category order by video_view_count) as rn
+					, count(*) over(partition by time_category) as total_count
+				from buckets 
+)
+
 select 
-	 case 	
-		when video_duration_sec < 60 then 'less than 1 min'
-		when video_duration_sec between 60 and 300 then '1-5 min'
-		when video_duration_sec between 301 and 600 then '5-10 min'
-		when video_duration_sec between 601 and 1800 then '10-30 min'
-		when video_duration_sec between 1801 and 3600 then '30-60 min'
-		when video_duration_sec between 3601 and 7200 then '1-2 hours'
-		when video_duration_sec between 7201 and 10800 then '2-3 hours'
-		when video_duration_sec between 10801 and 21600 then '3-6 hours'
-		when video_duration_sec > 21600  then '6 hours +'
-	end as time_category
-	, count(*) as videos_count
+	time_category
+	, count(*) as num_of_videos
 	, round(avg(video_view_count),0) as avg_views
+	, avg(case 
+			when rn between total_count/2.0 and total_count/ 2.0 + 1 then video_view_count 
+	end) as median_views
 	, round(avg(video_like_count),0) as avg_likes
 	, round(avg(video_comment_count),0) as avg_comments
-from youtube_trending_videos_global ytvg 
+from ranked_table
 group by time_category
-order by 2 desc
+order by num_of_videos desc
 ;
 
 -- #3 Minimum subscribers, views, and videos to get trending status
